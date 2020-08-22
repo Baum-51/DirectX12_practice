309062075c5b356b60db440af420c3c1d68f347f
Baum-51 <olympos.12z@gmail.com>
2020-08-22 23:03:18 +0900

    commit -p79

--- DirectX12_practice/main.cpp
+++ DirectX12_practice/main.cpp
@@ -1,29 +1,214 @@
+#include <Windows.h>
 #include <vector>
-#include <functional>
+#ifdef _DEBUG
 #include <iostream>
+#endif
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "dxgi.lib")
+
+#define window_width 500
+#define window_height 500
 
 using namespace std;
 
-int main() {
-	std::vector<std::function<void(void)>> commandlist;//コマンドリストを模したもの
+//@brief コンソール画面にフォーマット付き文字列を表示
+//@param formatフォーマット(%dとか%fとかの)
+//@param 可変長引数
+//@remarks この関数はデバッグ用です。デバッグ時にしか動作しません
 
-	commandlist.push_back([]() {cout << "GPU Set RTV-①" << endl; });
-	cout << "CPU Set命令-②" << endl;
+ID3D12Device* _dev = nullptr;
+IDXGIFactory6* _dxgiFactory = nullptr;
+IDXGISwapChain4* _swapchain = nullptr;
 
-	commandlist.push_back([]() {cout << "GPU Clear RTV-③" << endl; });
-	cout << "CPU Clear命令-④" << endl;
+auto result = CreateDXGIFactory(IID_PPV_ARGS(&_dxgiFactory));
 
-	commandlist.push_back([]() {cout << "GPU Close-⑤" << endl; });
-	cout << "CPU Close命令-⑥" << endl;
 
-	cout << endl;
-	//コマンドキューのExecuteCommandを模した処理
-	for (auto& cmd : commandlist)
+
+void DebugOutputFormatString(const char* format, ...)
+{
+#ifdef _DEBUG
+	va_list valist;
+	va_start(valist, format);
+	printf(format, valist);
+	va_end(valist);
+#endif
+}
+
+LRESULT WindowProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+
+	ID3D12CommandAllocator* _cmdAllocator = nullptr;
+	ID3D12GraphicsCommandList* _cmdList = nullptr;
+	ID3D12CommandQueue* _cmdQueue = nullptr;
+	D3D12_COMMAND_QUEUE_DESC cmdQueueDesc = {};
+	DXGI_SWAP_CHAIN_DESC1 swapchainDesc = {};
+
+	result = _dev->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&_cmdAllocator));
+	//cout << result << endl;
+	result = _dev->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, _cmdAllocator, nullptr, IID_PPV_ARGS(&_cmdList));
+	//cout << result << endl;
+
+	//タイムアウトなし
+	cmdQueueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+	//アダプターを１つしか使わないときは0でよい
+	cmdQueueDesc.NodeMask = 0;
+
+	//プライオリティは特に指定なし
+	cmdQueueDesc.Priority = D3D12_COMMAND_QUEUE_PRIORITY_NORMAL;
+	//コマンドリストと合わせる
+	cmdQueueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
+	//キュー生成
+	result = _dev->CreateCommandQueue(&cmdQueueDesc, IID_PPV_ARGS(&_cmdQueue));
+
+	swapchainDesc.Width = window_width;
+	swapchainDesc.Height = window_height;
+	swapchainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	swapchainDesc.Stereo = false;
+	swapchainDesc.SampleDesc.Count = 1;
+	swapchainDesc.SampleDesc.Quality = 0;
+	swapchainDesc.BufferUsage = DXGI_USAGE_BACK_BUFFER;
+	swapchainDesc.BufferCount = 2;
+
+	//バックバッファーは伸び縮み可能
+	swapchainDesc.Scaling = DXGI_SCALING_STRETCH;
+
+	//フィリップ後は速やかに破棄
+	swapchainDesc.AlphaMode = DXGI_ALPHA_MODE_UNSPECIFIED;
+
+	//ウィンドウ⇔フルスクリーン切り替え可能
+	swapchainDesc.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+
+
+	result = _dxgiFactory->CreateSwapChainForHwnd(
+		_cmdQueue,
+		hwnd,
+		&swapchainDesc,
+		nullptr,
+		nullptr,
+		(IDXGISwapChain1**)&_swapchain
+	);
+
+	//ウインドウが破棄されるときに呼び出される
+	if (msg == WM_DESTROY)
 	{
-		cmd();
+		PostQuitMessage(0);//OSにアプリケーションが終了をすることを伝える
+		return 0;
+	}
+	return DefWindowProc(hwnd, msg, wparam, lparam);//既定の処理を行う
+}
+
+#ifdef _DEBUG
+int main()
+{
+#else
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+#endif
+
+	D3D_FEATURE_LEVEL levels[] =
+	{
+		D3D_FEATURE_LEVEL_12_1,
+		D3D_FEATURE_LEVEL_12_0,
+		D3D_FEATURE_LEVEL_11_1,
+		D3D_FEATURE_LEVEL_11_0,
+	};
+
+	//Deirect3Dデバイスの初期化
+	D3D_FEATURE_LEVEL featureLevel;
+
+	for (auto lv : levels) {
+		if (D3D12CreateDevice(nullptr, lv, IID_PPV_ARGS(&_dev)) == S_OK)
+		{
+			featureLevel = lv;
+			break;//生成可能なバージョンを見つけたらループを打ち切り
+		}
 	}
 
+	//アダプターの列挙用
+	std::vector <IDXGIAdapter*> adapters;
+	//ここに特定の名前を持つアダプターオブジェクトが入る
+	IDXGIAdapter* tmpAdapter = nullptr;
+
+	for (int i = 0;
+		_dxgiFactory->EnumAdapters(i, &tmpAdapter) != DXGI_ERROR_NOT_FOUND;
+		++i)
+	{
+		adapters.push_back(tmpAdapter);
+	}
+	for (auto adpt : adapters)
+	{
+		DXGI_ADAPTER_DESC adesc = {};
+		adpt->GetDesc(&adesc);//アダプターの説明オブジェクト取得
+
+		std::wstring strDesc = adesc.Description;
+
+		//探したいアダプターの名前を確認
+		if (strDesc.find(L"NVIDIA") != string::npos)
+		{
+			tmpAdapter = adpt;
+			break;
+		}
+
+	}
+
+
+	
+
+	WNDCLASSEX winc = {};
+
+	winc.cbSize = sizeof(WNDCLASSEX);
+	winc.lpfnWndProc = (WNDPROC)WindowProcedure;//コールバック関数の指定
+	winc.lpszClassName = TEXT("DX12Sample");//アプリケーションクラス名
+	winc.hInstance = GetModuleHandle(nullptr);//ハンドルの取得
+
+	
+
+
+	RegisterClassEx(&winc);//アプリケーションクラス（ウィンドウクラスの指定をOSに伝える）
+
+	RECT wrc = { 0, 0, window_width, window_height };//ウインドウサイズを決める
+
+	//関数を使ってウィンドウのサイズを補正する
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+
+	//ウィンドウオブジェクトの生成
+	HWND hwnd = CreateWindow(winc.lpszClassName,//クラス名指定
+		TEXT("DX12テスト"),//タイトルバーの文字
+		WS_OVERLAPPEDWINDOW,//タイトルバーと境界線があるウィンドウ
+		CW_USEDEFAULT,//表示X座標はOSにお任せ
+		CW_USEDEFAULT,//表示Y座標はOSにお任せ
+		wrc.right - wrc.left,//ウィンドウ幅
+		wrc.bottom - wrc.top,//ウィンドウ高
+		nullptr,//親ウィンドウハンドル
+		nullptr,//メニューハンドル
+		winc.hInstance,//呼び出しアプリケーションハンドル
+		nullptr);//追加パラメータ
+
+	//ウィンドウ表示
+	ShowWindow(hwnd, SW_SHOW);
+
+	MSG msg = {};
+
+	while (true) {
+		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		//アプリケーションが終わるときにmessageがWM_QUITになる
+		if (msg.message == WM_QUIT)
+		{
+			break;
+		}
+	}
+
+	
+
+	DebugOutputFormatString("Breaka Window.");
 	getchar();
 
+	
 	return 0;
 }
